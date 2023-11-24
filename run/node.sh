ab08fa65371b0f6cf491d61f73f9be6f7155f2ad
bjarki <bjg60@hi.is>
2023-11-24 13:00:39 +0000

    remove custom basepath

--- run/node.sh
+++ run/node.sh
@@ -4,6 +4,6 @@ pushd ../substrate/bin/node/cli
 cargo build -r --bin substrate-node
 popd
 
-../target/release/substrate-node --base-path ~/.local/share/blockchain --chain=smly3 --validator --name "`hostname -s`" --bootnodes=/dns/twserver.rhi.hi.is/tcp/30333/p2p/12D3KooWHDwoQtc7aGczmr4oQEknN7csyKbCFtngyzJYPCYdTsGt $*
+../target/release/substrate-node --chain=smly3 --validator --name "`hostname -s`" --bootnodes=/dns/twserver.rhi.hi.is/tcp/30333/p2p/12D3KooWHDwoQtc7aGczmr4oQEknN7csyKbCFtngyzJYPCYdTsGt $*
 
 
